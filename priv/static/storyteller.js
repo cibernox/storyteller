9662b308a47717c2612bc3106036927a8431e577
Miguel <Miguel@MacBook-Pro-de-TBbackend.local>
2013-04-29 03:34:00 +0200

    Done. Using server side events

--- priv/static/storyteller.js
+++ priv/static/storyteller.js
@@ -1,10 +1,12 @@
 $(function(){
   $('.play').on('click', function(ev){
     ev.preventDefault();
-    var xhr = new XMLHttpRequest()
-    xhr.open("GET", $(this).href, true)
-    xhr.onprogress = function () {
-      $('#chat').append('<p class="story-line">' + xhr.responseText + '</p>');
-    }
+    var source = new EventSource(this.href);
+    source.addEventListener('message', function(e) {
+      var $line = $('<p class="story-line">' + e.data + '</p>');
+      $line.appendTo('#chat').hide().fadeIn(200, function(){
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+      })
+    }, false);
   });
 });
--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -5,6 +5,7 @@ defmodule ApplicationRouter do
     # Pick which parts of the request you want to fetch
     # You can comment the line below if you don't need
     # any of them or move them to a forwarded router
+    conn = conn.assign :layout, "layout"
     conn.fetch([:cookies, :params])
   end
 
@@ -13,24 +14,34 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Story Teller v0.1!")
+    conn = conn.assign(:title, "Welcome to Concurrent Story Teller v0.1!")
     render conn, "index.html"
   end
 
-  get "/story1" do
-    conn = conn.assign(:title, "Story1")
-    render conn, "story.html"
+  get "/:story_name" do
+    normalized_title = String.capitalize(String.replace(conn.params[:story_name], "-", " "))
+    conn = conn.assign :title, normalized_title
+    render conn, "#{conn.params[:story_name]}.html"
   end
 
-  get "/story1/play" do
+  get "/play/:story_name" do
+    conn = conn.resp_content_type("text/event-stream")
     conn = conn.send_chunked(200)
-    conn.chunk("Animal")
-    conn.chunk("Bellota")
-    conn.chunk("Calasparra")
-    conn.chunk("Dadivoso")
-    conn.chunk("Emergencia")
-    conn.chunk("Faisan")
-    conn.chunk("Galicia")
-    conn.chunk("Hipopotamo")
+
+    iterator = File.iterator!("#{conn.params[:story_name]}.txt")
+
+    Enum.each iterator, fn(line) ->
+      { :ok, conn } = conn.chunk "data: #{line}\n"
+      await conn, 1000, on_wake_up(&1, &2), on_time_out(&1)
+    end
+    conn
+  end
+
+  defp on_wake_up(arg1, arg2) do
+    # Nothing
+  end
+
+  defp on_time_out(arg1) do
+    # Nothing
   end
 end
